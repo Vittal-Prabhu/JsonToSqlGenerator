51f2375fdf3e7bddec1fda2d5c378c14ea0f3e87
Vittal Srinivas Prabhu <vittalsprabhu@gmail.com>
2023-11-17 02:38:22 +0530

    Commit representing Problem 2 specified in the requirement - Multi table sql query

--- app/src/main/java/jsontosql/App.java
+++ app/src/main/java/jsontosql/App.java
@@ -1,5 +1,6 @@
 /*
  * Java application to convert JSON file to Sql query
+ * Application further optimized in this code to support qurying from multiple tables - (Solution to Problem 2) 
  */
 package jsontosql;
 
@@ -8,32 +9,100 @@ import java.nio.file.Files;
 import java.net.URL;
 import org.json.JSONArray;
 import org.json.JSONObject;
+
 import java.util.HashSet;
 import java.util.Set;
 
 class JsonToSql {
     private StringBuilder sqlQuery; //Mutable field used build sql query
     private Set<String> usedOperators; // Track operators used for each field
+    private Set<String> tableNameSet; //Set of table names
+    private Set<String> joinTableNameSet; // Set of table names that are joined
 
     public JsonToSql() {
         this.sqlQuery = new StringBuilder();
         this.usedOperators = new HashSet<>();
+        this.tableNameSet = new HashSet<>();
+        this.joinTableNameSet = new HashSet<>();
     }
     
     // Function to build query from columns field fetched from JSON as JSONArray
-    public void buildQuery(JSONArray columns) {
-        sqlQuery.append("SELECT * FROM your_table WHERE ");
+    public void buildQuery(JSONObject jsonQuery) {
+        sqlQuery.append("SELECT * FROM ");
 
-        for (int i = 0; i < columns.length(); i++) {
-            JSONObject column = columns.getJSONObject(i);
-            String operator = column.getString("operator");
-            String fieldName = column.getString("fieldName");
-            Object fieldValue = column.get("fieldValue");
+        JSONArray tables = jsonQuery.getJSONArray("tables");
+        for (int i = 0; i < tables.length(); i++) {
+            JSONObject table = tables.getJSONObject(i);
+            String tableName = table.getString("tableName");
+            tableNameSet.add(tableName);
+
+            if(i == 0){
+                sqlQuery.append(tableName);
+            }
+
+            if (table.has("joinCondition")) {
+                JSONObject joinCondition = table.getJSONObject("joinCondition");
+                buildJoinCondition(tableName, joinCondition);
+            }
+
+            if (i < tables.length() - 1) {
+                sqlQuery.append(" ");
+            }
+        }
+
+        if (jsonQuery.has("whereCondition")) {
+            JSONObject whereCondition = jsonQuery.getJSONObject("whereCondition");
+            buildWhereCondition(whereCondition);
+        }
+
+        // Function to check if the table names specified in the join condition are valid
+        if(joinTableNameSet.size()>0){
+            if(!tableNameSet.containsAll(joinTableNameSet)){
+                throw new IllegalArgumentException("Incorrect join table specified");
+            }
+        }
+    }
+
+
+    // Function to build join condition
+    private void buildJoinCondition(String tableName, JSONObject joinCondition) {
+        sqlQuery.append("\nJOIN ");
+        String joinTable = joinCondition.getString("join");
+        joinTableNameSet.add(joinTable);
+        sqlQuery.append( joinTable);
+        sqlQuery.append(" ON ");
+        JSONArray conditions = joinCondition.getJSONArray("conditions");
+
+        for (int i = 0; i < conditions.length(); i++) {
+            JSONObject condition = conditions.getJSONObject(i);
+            String fieldName = condition.getString("fieldName");
+            String operator = condition.getString("operator");
+            String fieldValue = condition.getString("fieldValue");
+
+            sqlQuery.append(tableName).append(".").append(fieldName).append(" ").append(operator).append(" ").append(joinTable).append(".").append(fieldValue);
+
+            if (i < conditions.length() - 1) {
+                sqlQuery.append(" AND ");
+            }
+        }
+    }
+
+
+    // Function to build 'WHERE' condition
+    private void buildWhereCondition(JSONObject whereCondition) {
+        sqlQuery.append("\nWHERE ");
+        JSONArray conditions = whereCondition.getJSONArray("conditions");
+
+        for (int i = 0; i < conditions.length(); i++) {
+            JSONObject condition = conditions.getJSONObject(i);
+            String fieldName = condition.getString("fieldName");
+            String operator = condition.getString("operator");
+            String fieldValue = condition.getString("fieldValue");
 
             checkDuplicateOperator(operator, fieldName);
             appendCondition(operator, fieldName, fieldValue);
 
-            if (i < columns.length() - 1) {
+            if (i < conditions.length() - 1) {
                 sqlQuery.append(" AND ");
             }
         }
@@ -61,6 +130,9 @@ class JsonToSql {
                 sqlQuery.append(fieldName).append(" LIKE '").append(fieldValue).append("'");
                 break;
             case "EQUAL":
+                sqlQuery.append(fieldName).append(" = '").append(fieldValue).append("'");
+                break;
+            case "=":
                 sqlQuery.append(fieldName).append(" = '").append(fieldValue).append("'");
                 break;
             case ">":
@@ -127,21 +199,20 @@ public class App {
     public static void main(String[] args) {
         try {
             // Read JSON from file stored in resources directory
-            URL fileURL = App.class.getClassLoader().getResource("testJson1.json");
+            URL fileURL = App.class.getClassLoader().getResource("testJson2.json");
             File jsonFile = new File(fileURL.getFile());
             String jsonContent = new String(Files.readAllBytes(jsonFile.toPath()));
 
             // Parse JSON
             JSONObject jsonObject = new JSONObject(jsonContent);
-            JSONArray columns = jsonObject.getJSONArray("columns");
 
             // Build SQL query
             JsonToSql jsonToSql = new JsonToSql();
-            jsonToSql.buildQuery(columns);
+            jsonToSql.buildQuery(jsonObject);
             String sqlQuery = jsonToSql.getSqlQuery();
 
             // Output SQL query
-            System.out.println("Generated SQL Query:\n" + sqlQuery);
+            System.out.println("Generated SQL Query:\n \n" + sqlQuery);
         } catch (Exception e) {
             e.printStackTrace();
         }
